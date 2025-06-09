d568ae040c0b56b5e44eef2b25c2b672c2b58b72
JakeP <jakepfitsch@gmail.com>
2025-06-09 11:35:24 -1000

    changed contrast so checkboxes are easy to see

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1244,7 +1244,7 @@ class MainWindow(BoxLayout):
             # Show success message
             success_popup = Popup(
                 title='Success',
-                content=Label(text='Calculated values stored successfully, press Escape twice to return to main menu'),
+                content=Label(text='Calculated values stored successfully, press Escape three times to return to main menu'),
                 size_hint=(None, None),
                 size=(300, 150)
             )
